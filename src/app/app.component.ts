b7126b2e06ed84a8f3021ed22342841725dc8d8c
Akanksha <akankshapatil6687@gmail.com>
2023-10-05 22:59:07 +0530

    property binding and toggle state completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assignment-2';
+  inputText: string = ''; // Initialize with an empty string
+  displayedContent: string = '';
+
+  displayContent(value: string) {
+    this.displayedContent = `You entered: ${value}`;
+  }
+
+  isSuccess: boolean = true;
+  isError: boolean = false;
+  message: string = 'Success';
+
+  toggleErrorState() {
+    this.isSuccess = !this.isSuccess;
+    this.isError = !this.isError;
+    this.message = this.isError ? 'Error' : 'Success';
+  }
 }
